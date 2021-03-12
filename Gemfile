bc2d5d950e671a1ba380271268c303dab9198607
George <71434803+george-swift@users.noreply.github.com>
2021-03-12 10:56:27 +0100

    Set up linters locally

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'jbuilder', '~> 2.7'
 
 gem 'bootstrap-sass', '~> 3.4', '>= 3.4.1'
 
+gem 'rubocop', '~>0.81.0'
+
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :edit, :update]
-  get '/users/:id', to: 'users#edit'
+  get 'users/:id', to: 'users#edit'
 end
